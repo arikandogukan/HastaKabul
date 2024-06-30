03e74c57a879c08a3ab277febc4c85f324b4b273
ahmet <aheksi53@gmail.com>
2024-06-30 17:58:35 +0300

    Add project files.

---
+++ HastaKabul/Models/Hastalar.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastaKabul.Models
+{
+    public class Hastalar
+    {
+        [Key]
+        public int HastaId { get; set; }
+        public int HastaTc { get; set; }
+        public String HastaAd { get; set; }
+        public int HastaYas { get; set; }
+        public String HastaEmail { get; set; }
+        public String HastaPassword { get; set; }
+    }
+}
